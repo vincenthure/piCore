ddd8c56e50d33b8ff41d5eff1fc761f6d2c191f2
Postmedias <vincenthure@mac.com>
2021-12-30 16:05:02 +0100

    Create Rtimulib2_build.sh

---
+++ Rtimulib2_build.sh
@@ -0,0 +1,34 @@
+########### load dependency ############
+
+tce-load -i comiletc
+tce-load -i python3.8-dev
+tce-load -i squashfs-tools
+
+########### compile #########
+
+cd RTIMULib2-master/Linux/python
+python3 setup.py build
+sudo python3 setup.py install
+
+########### prepare tempory folder ###########
+
+cd /tmp
+mkdir -p rtimulib/usr/local/lib/python3.8/site-packages
+
+########### move file to tempory folder #############
+
+cd /usr/local/lib/python3.8/site-packages
+sudo mv RTI* /tmp/rtimulib/usr/local/lib/python3.8/site-packages
+cd /tmp
+ls rtimulib/usr/local/lib/python3.8/site-packages
+
+################ make tcz file ##############
+
+mksquashfs rtimulib rtimulib.tcz
+cp -v rtimulib.tcz /etc/sysconfig/tcedir/optional
+
+############## add to list to load on boot ############
+
+cd /etc/sysconfig/tcedir
+nano onboot.lst
+filetool.sh -b
