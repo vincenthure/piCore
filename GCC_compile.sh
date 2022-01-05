386408a3f5f3237c12d6029f75fe5a853ec60fd1
Postmedias <vincenthure@mac.com>
2022-01-05 12:41:38 +0100

    Update GCC_compile.sh

--- GCC_compile.sh
+++ GCC_compile.sh
@@ -6,17 +6,17 @@ sudo git clone git://gcc.gnu.org/git/gcc.git
 sudo scp vincent@192.168.0.15:/Volumes/DATA/vincent/gcc.zip  gcc.zip
 sudo unzip gcc.zip
 
-tce-load -iw compiletc perl5 ncursesw-dev bash mpc-dev udev-lib-dev texinfo coreutils glibc_apps rsync gettext python3.6 git 
-tce-load -iw mpc-dev gmp-dev mpfr-dev
+tce-load -i compiletc perl5 bash mpc-dev udev-lib-dev texinfo coreutils glibc_apps rsync gettext python3.6 git 
+tce-load -i mpc-dev gmp-dev mpfr-dev
 cd /mnt/mmcblk0p2
 
 ######### expand swap file #############
  
-dd if=/dev/zero of=swapfile1 bs=1024 count=102400
-chown root:root swapfile1
-chmod 0600 swapfile1
-mkswap swapfile1
-swapon swapfile1
+sudo dd if=/dev/zero of=swapfile1 bs=1024 count=1024000
+sudo chown root:root swapfile1
+sudo chmod 0600 swapfile1
+sudo mkswap swapfile1
+sudo swapon swapfile1
 free -m
 
 ########## compile #################
@@ -27,8 +27,7 @@ cd build
 export CFLAGS="-march=armv6zk -mtune=arm1176jzf-s -mfpu=vfp"
 export CXXFLAGS="-march=armv6zk -mtune=arm1176jzf-s -mfpu=vfp"
 
-sudo ./configure --prefix=/usr/local --enable-languages=c,c++,fortran --disable-multili
-b --disable-bootstrap --with-system-zlib --libexecdir=/usr/local/lib
+sudo ./configure --prefix=/usr/local --enable-languages=c,c++,fortran --disable-multilib --disable-bootstrap --with-system-zlib --libexecdir=/usr/local/lib
 
- make -j 4  DESTDIR=/mnt/mmcblk0p2/build
+sudo make -j 4  DESTDIR=/mnt/mmcblk0p2/build
 
